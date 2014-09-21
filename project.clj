5f1b063c797d7f290e92c3fa4f5b6410af4e37e8
soasme <soasme@gmail.com>
2014-09-21 22:26:45 +0800

    Proxy stats to browser.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
+                 [ring/ring-json "0.3.1"]
                  [beanstalk-clj "0.1.2"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler beanstalk-console.handler/app}
--- src/beanstalk_console/handler.clj
+++ src/beanstalk_console/handler.clj
@@ -1,23 +1,42 @@
 (ns beanstalk-console.handler
+  (:use [ring.middleware params cookies]
+        [ring.util.response])
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.middleware.json :as json-middleware]
             [beanstalk-clj.core :as beanstalk]))
 
+(defn stats-job [config jid]
+  (let [client (beanstalk/beanstalkd-factory config)
+        stats (beanstalk/stats-job client jid)]
+    (response stats)))
+
+(defn stats-server [config]
+  (let [client (beanstalk/beanstalkd-factory config)
+        stats (beanstalk/stats-beanstalkd client)]
+    (response stats)))
+
+(defn stats-tube [config tube-name]
+  (let [client (beanstalk/beanstalkd-factory config)
+        stats (beanstalk/stats-tube client tube-name)]
+    (response stats)))
+
+(defn list-tubes [config]
+  (let [client (beanstalk/beanstalkd-factory config)
+        tubes (beanstalk/list-tubes client)]
+    (prn tubes)
+    (response tubes)))
 
 (defroutes app-routes
 
   ; API
   (context
    "/api" []
-   (GET "/stats-job" []
-        "{}")
-   (GET "/stats-tube" []
-        "{}")
-   (GET "/stats-beanstalkd" []
-        "{}")
-   (GET "/list-tubes" []
-        "[]"))
+   (GET "/:config/stats-job/:jid" [config jid] (stats-job config jid))
+   (GET "/:config/stats-tube/:tube-name" [config tube-name] (stats-tube config tube-name))
+   (GET "/:config/stats" [config] (stats-server config))
+   (GET "/:config/list-tubes" [config] (list-tubes config)))
 
   ; Index Page
   (GET "/" [] "Hello World")
@@ -26,4 +45,6 @@
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (-> (handler/site app-routes)
+      (json-middleware/wrap-json-body)
+      (json-middleware/wrap-json-response)))

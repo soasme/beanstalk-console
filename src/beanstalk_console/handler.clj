5c347c8456d842acefe71fcd53b44a3705c78b27
soasme <soasme@gmail.com>
2014-09-21 22:01:05 +0800

    Define api routes.

--- src/beanstalk_console/handler.clj
+++ src/beanstalk_console/handler.clj
@@ -1,11 +1,28 @@
 (ns beanstalk-console.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [beanstalk-clj.core :as beanstalk]))
+
 
 (defroutes app-routes
+
+  ; API
+  (context
+   "/api" []
+   (GET "/stats-job" []
+        "{}")
+   (GET "/stats-tube" []
+        "{}")
+   (GET "/stats-beanstalkd" []
+        "{}")
+   (GET "/list-tubes" []
+        "[]"))
+
+  ; Index Page
   (GET "/" [] "Hello World")
   (route/resources "/")
+
   (route/not-found "Not Found"))
 
 (def app
